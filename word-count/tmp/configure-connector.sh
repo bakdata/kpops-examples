0eabd38fe5eca3741eaccc4bbc79e8f3f6cdb1aa
MichaelKora <michael.koras0@gmail.com>
2023-01-30 17:39:10 +0100

    Clean code using review

--- word-count/tmp/configure-connector.sh
+++
@@ -1,1 +0,0 @@
-curl -s -X POST -H 'Content-Type: application/json' --data @kafka-connect-redis.json http://localhost:8083/connectors
--- word-count/tmp/deploy-pipeline.sh
+++ word-count/tmp/deploy-pipeline.sh
@@ -3,4 +3,5 @@
 helm upgrade --debug --install  --values values-dataproducer.yaml  --kube-context gke_gcp-bakdata-cluster_us-east1_gcp-bakdata-dev-cluster --namespace kpops-word-count wc-data-producer bakdata-common/producer-app
 helm upgrade --debug --install  --values values-wordcounter.yaml --kube-context gke_gcp-bakdata-cluster_us-east1_gcp-bakdata-dev-cluster --namespace kpops-word-count  wc-word-counter bakdata-common/streams-app
 
-# sh configure-connector.sh
+# curl -s -X POST -H 'Content-Type: application/json' --data @kafka-connect-redis.json http://localhost:8083/connectors
+# curl -X DELETE http://localhost:8083/connectors/RedisSinkConnector
