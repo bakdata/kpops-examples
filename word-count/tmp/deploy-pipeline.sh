60a9c4f6b6c0e3ed792a613964b497c3fd830bd8
MichaelKora <michael.koras0@gmail.com>
2023-02-06 11:03:52 +0100

    Remove tmp folder

--- word-count/tmp/deploy-pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-helm repo add bakdata-common https://bakdata.github.io/streams-bootstrap/
-helm repo update
-helm upgrade --debug --install  --values values-dataproducer.yaml   --namespace kpops-word-count wc-data-producer bakdata-common/producer-app
-helm upgrade --debug --install  --values values-wordcounter.yaml  --namespace kpops-word-count  wc-word-counter bakdata-common/streams-app
--- word-count/tmp/uninstall-pipeline.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-helm uninstall  --namespace kpops-word-count wc-data-producer
-helm uninstall  --namespace kpops-word-count  wc-word-counter
