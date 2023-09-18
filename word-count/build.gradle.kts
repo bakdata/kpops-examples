9fa1039aec922f53e493549ce41e5f3f89e9e978
Ramin Gharib <ramin.gharib@bakdata.com>
2023-09-18 15:00:18 +0200

    Update streams bootstrap

--- word-count/build.gradle.kts
+++ word-count/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     java
     idea
     id("io.freefair.lombok") version "6.6.1"
-    id("com.google.cloud.tools.jib") version "3.3.1"
+    id("com.google.cloud.tools.jib") version "3.3.2"
 }
 
 group = "com.bakdata.kpops.examples"
@@ -11,6 +11,7 @@ group = "com.bakdata.kpops.examples"
 repositories {
     mavenCentral()
     maven(url = "https://packages.confluent.io/maven/")
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 
@@ -32,7 +33,7 @@ tasks {
 }
 
 dependencies {
-    implementation(group = "com.bakdata.kafka", name = "streams-bootstrap", version = "2.8.0")
+    implementation(group = "com.bakdata.kafka", name = "streams-bootstrap", version = "2.12.2-SNAPSHOT")
     implementation(group = "org.apache.logging.log4j", name = "log4j-slf4j-impl", version = "2.19.0")
     implementation(group = "com.google.guava", name = "guava", version = "31.1-jre")
 
@@ -40,7 +41,7 @@ dependencies {
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
     testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
     testImplementation(group = "org.assertj", name = "assertj-core", version = "3.24.2")
-    val fluentKafkaVersion = "2.8.1"
+    val fluentKafkaVersion = "2.9.0"
     testImplementation(
         group = "com.bakdata.fluent-kafka-streams-tests",
         name = "fluent-kafka-streams-tests-junit5",
@@ -49,11 +50,5 @@ dependencies {
     val kafkaVersion: String by project
     testImplementation(group = "net.mguenther.kafka", name = "kafka-junit", version = kafkaVersion) {
         exclude(group = "org.slf4j", module = "slf4j-log4j12")
-    }
-}
-
-jib {
-    from {
-        image = "eclipse-temurin:17.0.6_10-jre"
     }
 }
