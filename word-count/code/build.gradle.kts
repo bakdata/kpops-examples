9dac8da10a106b6cb8771afb06549e8ccc206925
Ramin Gharib <ramin.gharib@bakdata.com>
2024-12-12 12:54:08 +0100

    Update files

--- word-count/code/build.gradle.kts
+++ word-count/code/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     id("com.bakdata.release") version "1.4.1"
     id("com.bakdata.sonar") version "1.4.1"
     id("com.bakdata.sonatype") version "1.4.1"
-    id("org.hildan.github.changelog") version "2.2.0"
     id("io.freefair.lombok") version "8.11"
     id("com.google.cloud.tools.jib") version "3.4.4"
 }
